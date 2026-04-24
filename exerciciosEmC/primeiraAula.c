16be27538eea310c349f68cc7e107792006fd166
MarceloMSQ <128309570+MarceloMSQ@users.noreply.github.com>
2026-04-24 08:48:54 -0300

    strings usando gets

--- exerciciosEmC/primeiraAula.c
+++ exerciciosEmC/primeiraAula.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main() {
+	char nome[21], sobreNome[21];
+	printf("Primeiro nome: ");
+	gets(nome);
+	
+	printf("Ultimo sobrenome: ");
+	gets(sobreNome);
+	
+	printf("Ola senhor %s %s. Bem-vindo ao curso C Progressivo. \n", nome, sobreNome);
+	
+	return 0;
+}
