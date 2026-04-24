6dfa1e60ee407f5493ef7b25ed900caeffa66908
MarceloMSQ <128309570+MarceloMSQ@users.noreply.github.com>
2026-04-24 09:10:25 -0300

    Implement string copy from first name to surname
    Added string copy functionality to overwrite surname with first name.

--- exerciciosEmC/primeiraAula.c
+++ exerciciosEmC/primeiraAula.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+// strlen(); função retorna tamanho da string.
+//strcpy (dest, fonte); copia string contida na variável fonte para dest.
 
 int main() {
 	char nome[21], sobreNome[21];
@@ -9,9 +11,11 @@ int main() {
 	
 	printf("Ultimo sobrenome: ");
 	gets(sobreNome);
+	strcpy(sobreNome, nome); //copiar nome para sobrenome
 	
 	printf("Ola senhor %s %s. Bem-vindo ao curso C Progressivo. \n", nome, sobreNome);
 	printf("O nome tem tamanho %d", strlen(nome));
+	
 	
 	return 0;
 }
