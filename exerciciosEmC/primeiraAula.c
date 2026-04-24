3163e1ef462fad063ce6943a1eb586b6b88822ae
MarceloMSQ <128309570+MarceloMSQ@users.noreply.github.com>
2026-04-24 09:19:07 -0300

    Change sobrenome handling to concatenate with nome
    Updated the handling of the 'sobreNome' variable to concatenate 'nome' instead of copying it.

--- exerciciosEmC/primeiraAula.c
+++ exerciciosEmC/primeiraAula.c
@@ -3,6 +3,7 @@
 #include <string.h>
 // strlen(); função retorna tamanho da string.
 //strcpy (dest, fonte); copia string contida na variável fonte para dest.
+//strcat (dest, fonte); concatena string.
 
 int main() {
 	char nome[21], sobreNome[21];
@@ -11,7 +12,8 @@ int main() {
 	
 	printf("Ultimo sobrenome: ");
 	gets(sobreNome);
-	strcpy(sobreNome, nome); //copiar nome para sobrenome
+	//strcpy(sobreNome, nome); //copiar nome para sobrenome
+	strcat(sobreNome, nome);
 	
 	printf("Ola senhor %s %s. Bem-vindo ao curso C Progressivo. \n", nome, sobreNome);
 	printf("O nome tem tamanho %d", strlen(nome));
